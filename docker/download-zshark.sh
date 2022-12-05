1ce113978f27016a90f8cd8a19761c33d86a426c
Philippe Delodder <philippe@delodder.be>
2022-12-05 13:40:58 +0100

    Fix download URL

--- docker/download-zshark.sh
+++ docker/download-zshark.sh
@@ -7,11 +7,11 @@ PLATFORM=$2
 
 if echo "${PLATFORM}" | grep -qE "amd64";
 then
-  URL="http://phoscon.de/downloads/zshark/ubuntu/zshark-${ZSHARK_VERSION}.deb"
+  URL="http://phoscon.de/downloads/zshark/ubuntu/zshark-${ZSHARK_VERSION}-amd64.bionic.deb"
 fi
 if echo "${PLATFORM}" | grep -qE "v7";
 then
-  URL="http://phoscon.de/downloads/zshark/raspbian/zshark-${ZSHARK_VERSION}-amd64.bionic.deb"
+  URL="http://phoscon.de/downloads/zshark/raspbian/zshark-${ZSHARK_VERSION}.deb"
 fi
 
 curl -vv "${URL}" -o /zshark${DEV}.deb
